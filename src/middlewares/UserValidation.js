ff6956a796f203647e71db9ca5f0c0ff7f557ff5
Sebastiao <rodrigoexer1@gmail.com>
2023-06-08 17:12:03 -0300

    updated-user-validation

--- src/middlewares/UserValidation.js
+++ src/middlewares/UserValidation.js
@@ -3,8 +3,13 @@ const UserModel = require('../model/UserModel')
 const UserValidation = async(req, res, next) => {
     const { nome, email, senha } = req.body;
 
-    if(!nome){ return res.status(400).json({error: 'Não posso gravar sem o nome'})
-    }else{ 
+    if(!nome){
+        return res.status(400).json({error: 'Não posso gravar sem o nome valido!'})
+    } else if(!email){
+        return res.status(400).json({error: 'Não posso gravar sem o email valido!'})
+    } else if(!senha) {
+        return res.status(400).json({error: 'Não posso gravar sem o senha valida!'})
+    }else{
         next();
     }
 }
