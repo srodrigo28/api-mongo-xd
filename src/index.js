4263fa7c05bfb769fb0e88daaee46f455f1653c3
Sebastiao <rodrigoexer1@gmail.com>
2023-06-11 18:08:44 -0300

    update-api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const server = express()
+server.use(cors())
 server.use(express.json())
 
 const TaskRoutes = require('./routes/TaskRoutes');
