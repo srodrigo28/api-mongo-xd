a2979a83d47b8bb832c52c7c6f7bded742cff975
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-06-12 15:36:05 -0300

    update ate

--- src/routes/ContaRoutes.js
+++ src/routes/ContaRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const ContaController = require('../controller/ContaController')
 const ContaValidation = require('../middlewares/ContaValidation')
 
-router.post('/', ContaValidation, ContaController.create)
+router.post('/', ContaController.create)
 router.get('/', ContaController.all)
 router.get('/:id', ContaController.allById)
 router.delete('/:id', ContaController.delete)
